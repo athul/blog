edfecadc0856c4d97c6f801382357a59af167be9
Athul-CA <athul8720@gmail.com>
2019-06-22 13:07:09 +0530

    Removed bgcolor and added my name to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,14 +57,13 @@ class Layout extends React.Component {
           marginLeft: `auto`,
           marginRight: `auto`,
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-          background:`teal`, 
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}` 
         }}
       >
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built by Athul { with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
