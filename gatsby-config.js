02bebfe16d007e51e9cd9e6faa0a01f57a8b0696
Athul-CA <athul8720@gmail.com>
2019-06-22 22:17:49 +0530

    Updated favicon image

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#1abaa2`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/main-pic.jpg`,
       },
     },
     `gatsby-plugin-offline`,
