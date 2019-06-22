014ec58f46c4e1d49a23d2b801c663354ae3b055
Athul-CA <athul8720@gmail.com>
2019-06-22 08:47:39 +0530

    Trying to update the color of the blog title

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,10 @@ class BlogIndex extends React.Component {
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                <Link style={{ 
+			boxShadow: `none`,
+			color:`Green`,
+		}} to={node.fields.slug}>
                   {title}
                 </Link>
               </h3>
