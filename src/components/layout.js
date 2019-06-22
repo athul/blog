6bb945e991c27e7c53c1c770af49b14a079e4c81
Athul Cyriac Ajay <athul8720@gmail.com>
2019-06-22 09:00:44 +0530

    Changing Background to Black

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,6 +58,7 @@ class Layout extends React.Component {
           marginRight: `auto`,
           maxWidth: rhythm(24),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          background:`black`, 
         }}
       >
         <header>{header}</header>
