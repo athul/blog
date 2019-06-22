d4ce2413f463b5e36d0d996975930cb407264289
Athul-CA <athul8720@gmail.com>
2019-06-22 08:56:20 +0530

    To REBECCAPURPLE...

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ class BlogIndex extends React.Component {
               >
                 <Link style={{ 
 			boxShadow: `none`,
-			color:`Red`,
+			color:`rebeccapurple`,
 		}} to={node.fields.slug}>
                   {title}
                 </Link>
