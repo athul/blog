c0cd0f29d4b6cde1867fbdef26549cab9ae7eda8
Athul-CA <athul8720@gmail.com>
2019-06-23 07:11:26 +0530

    More syntax highlight plugin cutomization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import 'prismjs/themes/prism-tomorrow.css';
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
