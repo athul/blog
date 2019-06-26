1b20510995dcaadcb101c1f57787af7bebe2e52b
Athul-CA <athul8720@gmail.com>
2019-06-26 12:27:49 +0530

    Added styling to div

--- src/components/social.js
+++ src/components/social.js
@@ -1,9 +1,14 @@
 import React from "react"
 import SocialLogo from "social-logo"
 
+import { rhythm } from "../utils/typography"
+
 const Socials=()=>{
 return(
-<div>
+	<div style={{
+		display:`flex`,
+		marginBottom:rythm(2.5),
+		color:`azure`}}>
 	<span>
 <a href="https://github.com/Athul-CA"><SocialLogo icon="github" size={ 48 } /></a>
 	<a href="https://twitter.com/athulcajay"><SocialLogo icon="twitter" size={ 48 } /></a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Socials from "../components/social"
 import { rhythm } from "../utils/typography"
 
 class BlogIndex extends React.Component {
@@ -41,7 +42,8 @@ class BlogIndex extends React.Component {
               />
             </div>
           )
-        })}
+	})}
+	<Socials />
       </Layout>
     )
   }
