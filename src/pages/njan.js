3f03d192fe343bafe954d1c1778dede7775ff401
Athul-CA <athul8720@gmail.com>
2019-06-29 21:00:03 +0530

    Init njan

---
+++ src/pages/njan.js
@@ -0,0 +1,33 @@
+import React from "react"
+import {graphql} from "gatsby"
+import {rhythm} from "../utils/typography"
+import Layout from "../components/layout"
+
+class njan extends React.Component {
+	render(){
+		const {data}=this.props
+		const site.tle=data.site.siteMetadata.title
+		return(
+			<body style={{
+				background:`grey`
+				}}
+			<Layout location={this.props.location} title=`About me`>
+				<h1 style={{
+	marginBottom: rhythm(1/4),
+	color:`fuschia`
+				}}>I am Athul Cyriac Ajay</h1>
+			</Layout>
+		</body>
+		)
+	}
+}
+export default njan
+
+export const njanq=graphql`
+query{
+ site{
+  siteMetadata{
+   title
+   }
+  }
+}`
