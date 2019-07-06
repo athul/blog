235ffae029a0946c4a5e66fcae967a4f39593f3b
Athul-CA <athul8720@gmail.com>
2019-07-06 10:14:29 +0530

    removed layout

--- src/pages/njan.js
+++ src/pages/njan.js
@@ -1,10 +1,9 @@
 import React from "react"
 import {rhythm} from "../utils/typography"
 import Layout from "../components/layout"
-import Card from "rebass"   
+//import Card from "rebass"   
 
-class njan extends React.Component {
-	render(){
+class Njan extends React.Component {
 	return(
 			<body style={{
 				background:`grey`
@@ -15,20 +14,8 @@ class njan extends React.Component {
 	color:`white`}}>I am Athul Cyriac Ajay</h1>
 	<p>I am a <strong>GitHub Campus Expert</strong>🚩🚩,Hackathon Organizer, Developer, and moreover a Human Being😊</p> 
 	<p style={{color:`azure`}}> Made this Blog with The Blog Starter from Gatsby</p>
-	<Card
-		fontSize={6}
-  fontWeight='bold'
-  width={[ 1, 1, 1/2 ]}
-  p={5}
-  my={5}
-  bg='#f6f6ff'
-  borderRadius={8}
-  boxShadow='0 2px 16px rgba(0, 0, 0, 0.25)'> 
-		Some potential Hackathon Sponsors
-	</Card>
 		</body>
 		)
-	}
 }
 
-export default njan
+export default Njan
