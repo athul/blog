4bada6253584a62c08310a6728a22d46c3a25392
Athul Cyriac Ajay <athul8720@gmail.com>
2019-07-06 08:26:04 +0530

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,12 @@
+//Global CSS
+
+import '../src/styles/global.css'
+
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
 //Highlighter
 require("prismjs/themes/prism-solarizedlight.css")
-//Global CSS
-import '.src/styles/global.css'
 
 
