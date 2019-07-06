6eab5d374de0f061d25fc93021f8cb0fdcad7412
Athul-CA <athul8720@gmail.com>
2019-07-06 08:19:46 +0530

    typos

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,6 @@ import "typeface-merriweather"
 //Highlighter
 require("prismjs/themes/prism-solarizedlight.css")
 //Global CSS
-require('.src/styles/global.css')
+import '.src/styles/global.css'
+
 
