473232813438f1e9b1977c934758ab1d22205a96
athul <athul8720@gmail.com>
2019-09-01 19:15:59 +0530

    New Sponsor Links

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -44,8 +44,8 @@ class BlogPostTemplate extends React.Component {
             marginBottom: rhythm(1),
           }}
         />
-        <Bio />
         <a href='https://ko-fi.com/O5O6YTZI' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=2' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        <Bio />
         <ul
           style={{
             display: `flex`,
