6e6eb7853c5de3c12756e19406bbce924edb3739
Athul Cyriac Ajay <athul8720@gmail.com>
2019-12-26 23:05:30 +0530

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -44,7 +44,6 @@ class BlogPostTemplate extends React.Component {
             marginBottom: rhythm(1),
           }}
         />
-        <a href="https://www.buymeacoffee.com/JeVlc7T" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
         <Bio />
       <ul
           style={{
