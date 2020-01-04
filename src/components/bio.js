d3310be29161cb0e2b7c78c7a0da3ae10c01a403
Athul Cyriac Ajay <athul8720@gmail.com>
2020-01-04 07:01:35 +0530

    📝 Profile Pic Updates

--- src/components/bio.js
+++ src/components/bio.js
@@ -16,7 +16,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
+      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
         childImageSharp {
           fixed(width: 50, height: 50) {
             ...GatsbyImageSharpFixed
