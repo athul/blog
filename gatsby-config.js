285974e02c0dc1b171c6dcf48dc01c081046f3b0
Athul Cyriac Ajay <athul8720@gmail.com>
2020-01-05 10:46:14 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,16 +61,66 @@ module.exports = {
       },
     },
     `gatsby-plugin-feed`,
-    {
+{
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      },
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `dayDreams ++`,
-        short_name:`++`,
+        short_name: `++`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#1abaa2`,
         display: `minimal-ui`,
-	      icon: `content/assets/n1.png`,
+        icon: `content/assets/n1.png`,
       },
     },
     `gatsby-plugin-offline`,
