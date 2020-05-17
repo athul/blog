ee65bb316dc1df22245c9e231a0025a8f304a69f
athul <athul8720@gmail.com>
2020-05-17 21:32:32 +0530

    New Post on Termchat

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,5 +89,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-dark-mode',
+      {
+        resolve: `gatsby-remark-responsive-iframe`,
+        options: {
+          wrapperStyle: `margin-bottom: 1.0725rem`,
+        },
+      },
   ],
 }
